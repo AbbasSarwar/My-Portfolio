9f85ce70d2ff42f65711677dee9cd5ddd5766606
Abbas Sarwar <abbassarwar100@gmail.com>
2023-09-07 14:30:37 +0500

    Resume Link added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,12 +24,12 @@ const Navbar = ({sidebar, setSidebar}) => {
             </div>
             <nav>
                 <ul className="menu">
-                    <li><NavLink onClick={reset} to="/"><i className="fa-solid fa-house icon" /> <span className={styles.textsize}>Home</span></NavLink></li>
-                    <li><NavLink onClick={reset} to="/About"><i className="fa-solid fa-user icon" /> <span className={styles.textsize}>About</span></NavLink></li>
-                    <li><NavLink onClick={reset} to="/Resume"><i className="fa-solid fa-file icon" /> <span className={styles.textsize}>Resume</span></NavLink></li>
-                    <li><NavLink onClick={reset} to="/Portfolio"><i className="fa-solid fa-layer-group icon" /> <span className={styles.textsize}>Portfolio</span></NavLink></li>
-                    <li><NavLink onClick={reset} to="/Stacks"><i className="fa-solid fa-server icon" /> <span className={styles.textsize}>Stacks</span></NavLink></li>
-                    <li><NavLink onClick={reset} to="/Contact"><i className="fa-solid fa-envelope icon" /> <span className={styles.textsize}>Contact</span></NavLink></li>
+                    <li><NavLink className={styles.flexLink} onClick={reset} to="/"><i className="fa-solid fa-house col-1 icon" /> <span className={styles.textsize}>Home</span></NavLink></li>
+                    <li><NavLink className={styles.flexLink} onClick={reset} to="/About"><i className="fa-solid fa-user col-1 icon" /> <span className={styles.textsize}>About</span></NavLink></li>
+                    <li><NavLink className={styles.flexLink} onClick={reset} to="/Resume"><i className="fa-solid fa-file col-1 icon" /> <span className={styles.textsize}>Resume</span></NavLink></li>
+                    <li><NavLink className={styles.flexLink} onClick={reset} to="/Portfolio"><i className="fa-solid fa-layer-group col-1 icon" /> <span className={styles.textsize}>Portfolio</span></NavLink></li>
+                    <li><NavLink className={styles.flexLink} onClick={reset} to="/Stacks"><i className="fa-solid fa-server col-1 icon" /> <span className={styles.textsize}>Stacks</span></NavLink></li>
+                    <li><NavLink className={styles.flexLink} onClick={reset} to="/Contact"><i className="fa-solid fa-envelope col-1 icon" /> <span className={styles.textsize}>Contact</span></NavLink></li>
                 </ul>
             </nav>
             <div className="footer">
